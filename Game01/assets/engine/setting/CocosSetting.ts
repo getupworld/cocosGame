89c7da2f348139cc8354eb5f528087e76f96f1d5
lyj <296006763@qq.com>
2020-07-20 00:13:33 +0800

    update cocossetting module

--- Game01/assets/engine/setting/CocosSetting.ts
+++ Game01/assets/engine/setting/CocosSetting.ts
@@ -1,12 +1,13 @@
 
 export default class CocosSetting {
-    displayStats: boolean;
     Init(): void {
         this.SetDisplayStats(false);
     }
     // 设置左下角隐藏
     SetDisplayStats(sw: boolean): void {
         cc.debug.setDisplayStats(sw);
-        this.displayStats = sw;
+    }
+    IsDisplayStats(): boolean {
+        return cc.debug.isDisplayStats();
     }
 }
